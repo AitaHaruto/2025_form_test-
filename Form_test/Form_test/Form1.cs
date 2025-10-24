7a9b30bbb22e83394c04f0b6caf4523a23d30aaa
AitaHaruto <mazirenja0212@gmail.com>
2025-10-24 15:34:07 +0900

    4*6のボタンの作成

--- Form_test/Form_test/Form1.cs
+++ Form_test/Form_test/Form1.cs
@@ -11,10 +11,37 @@ using System.Windows.Forms;
 namespace Form_test
 {
     public partial class Form1 : Form
-    {
+    { 
+       
         public Form1()
         {
+            int cnt = 0;
             InitializeComponent();
+            for (int i = 1; i <= 4; i++)
+                
+            {
+                
+                for (int j = 1; j <= 6; j++)
+                {
+                    cnt = cnt + 1;
+                    TestButton testButton1 = new TestButton();
+
+
+                    //位置設定.
+                    testButton1.Location = new Point(50 * j, 50*i);
+
+                    //大きさ設定.
+                    testButton1.Size = new Size(50, 50);
+                    //テキストを設定.
+                    testButton1.Text = $"{cnt}";
+
+                    testButton1.BackColor = new Color();
+
+                    Controls.Add(testButton1);
+                }
+            }
+
         }
+        
     }
 }
